e882ce57a3e4d9caf2e20d7bc9e0abf279669800
John McCall <mcca0798@zytel.morris.umn.edu>
2013-02-27 16:52:11 -0600

    We wrote doFull which generates a random tree with a maximum depth that we specify.

--- EssentialsOfMetaheuristics/src/geneticTreeStructure/TreeBasedGP.groovy
+++ EssentialsOfMetaheuristics/src/geneticTreeStructure/TreeBasedGP.groovy
@@ -2,9 +2,21 @@ package geneticTreeStructure
 
 class TreeBasedGP {
 
-	def doFull(depth, max, functionSet){
+	def doFull(depth, max, functionSet, terminalSet){
 		if(depth >= max){
-			
+			getRandom(functionSet).copy()
+		} else {
+			node = getRandom(terminalSet).copy()
+			arity = node.function.getArity
+			arity.times{
+				node.addChild(doFull(depth+1, max, functionSet, terminalSet))
+			}
+			return node
 		}
+	}
+	
+	def getRandom(list){
+		Random rand = new Random()
+		node = list[rand.nextInt(list.size())].copy()
 	}
 }
