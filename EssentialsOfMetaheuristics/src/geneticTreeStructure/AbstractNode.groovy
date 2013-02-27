23a6ad089be3905ef8e9d1ec3fb85056d905f219
John McCall <mcca0798@avenger.morris.umn.edu>
2013-02-27 14:03:39 -0600

    Started the Genetic Tree Structure

---
+++ EssentialsOfMetaheuristics/src/geneticTreeStructure/AbstractNode.groovy
@@ -0,0 +1,32 @@
+package geneticTreeStructure
+
+class AbstractNode {
+	// This should be a closure(?)
+	def function
+	
+	// Going to be a constant OR a string variable
+	def value
+	
+	// This is a list of this nodes children
+	def children
+	
+	//TODO
+	def eval(variableMap) {
+		if(children.size() > 0) {
+			function.doMath(children[0].eval(variableMap),
+					   		children[1].eval(variableMap))
+		} else {
+			if(value instanceof String) {
+				variableMap.get(value)
+			} else {
+				value
+			}
+		}
+	}
+	
+	//TODO
+	String toString() {
+		
+	}
+	
+}
---
+++ EssentialsOfMetaheuristics/src/geneticTreeStructure/TreeBasedGP.groovy
@@ -0,0 +1,5 @@
+package geneticTreeStructure
+
+class TreeBasedGP {
+
+}
