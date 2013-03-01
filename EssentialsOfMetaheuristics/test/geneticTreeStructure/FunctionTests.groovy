224da663a23532cc0814fb9cb777448256baa20f
EmmaIreland <irela065@morris.umn.edu>
2013-02-28 18:47:14 -0600

    Added subtract test.

--- EssentialsOfMetaheuristics/test/geneticTreeStructure/FunctionTests.groovy
+++ EssentialsOfMetaheuristics/test/geneticTreeStructure/FunctionTests.groovy
@@ -1,15 +1,24 @@
 package geneticTreeStructure
 
 import functions.Add
+import functions.Subtract
 import spock.lang.Specification
 
 class FunctionTests extends Specification {
-	def "testing add"(){
-		def add = new Add()
-		expect:
-		add.doMath(3, 4) == 7
-		add.doMath(2, 10) == 12
-		add.doMath(-9, 4) == -5
-	}
+    def "testing add"(){
+        def add = new Add()
+        expect:
+        add.doMath(3, 4) == 7
+        add.doMath(2, 10) == 12
+        add.doMath(-9, 4) == -5
+    }
+
+    def "testing subtract"(){
+        def subtract = new Subtract()
+        expect:
+        subtract.doMath(3, 4) == -1
+        subtract.doMath(10, 2) == 8
+        subtract.doMath(9, -4) == 13
+    }
 
 }
