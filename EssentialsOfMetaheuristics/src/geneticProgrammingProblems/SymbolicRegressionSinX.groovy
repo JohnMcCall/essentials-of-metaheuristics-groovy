59c7385e0dbf974f562bbf4d9a1baf61282c0c46
John McCall <mcca0798@acrylic.morris.umn.edu>
2013-03-04 21:45:43 -0600

    Finished writing SymbolicRegressionSinX and its tests.

--- EssentialsOfMetaheuristics/src/geneticProgrammingProblems/SymbolicRegressionSinX.groovy
+++ EssentialsOfMetaheuristics/src/geneticProgrammingProblems/SymbolicRegressionSinX.groovy
@@ -6,7 +6,7 @@ import geneticTreeStructure.Mutation
 class SymbolicRegressionSinX {
 	Integer evalCount = 0
 	Integer maxIterations = 1000
-	Integer numPoints = 10
+	def numPoints = 10.0
 	def maximalQuality = 0 // the lower the quality the better for this problem
 	def mutator = new Mutation()
 	def geneticTree = new GeneticTree()
@@ -23,7 +23,7 @@ class SymbolicRegressionSinX {
 		// Create an array of results of calling our Tree with x-> every point
 		def treeArray = []
 		points.each {
-			tree.eval(["x" : it]) // assume x is the only variable
+			treeArray.add(tree.eval(["x" : it])) // assume x is the only variable
 		}
 		// Take the absolute value of the difference between the two at every point 
 		def absArray = []
@@ -32,13 +32,11 @@ class SymbolicRegressionSinX {
 			absArray.add(Math.abs(it - sinArray[count]))
 			count++
 		}
-		
 		// Take the average of these points to give us the quality
 		def result = 0
 		absArray.each {
 			result += it
 		}
-		
 		result/numPoints
 	}
 	
@@ -64,14 +62,13 @@ class SymbolicRegressionSinX {
 		this.class.name.split("\\.")[-1] + "_" + numPoints + "_" + maxIterations
 	}
 	
-	//TODO: Test this sucka
 	private getPoints() {
 		def toReturn = []
 		def interval = (2*Math.PI/numPoints)
 		def previous = 0
 		numPoints.times {
 			toReturn.add(interval + previous)
-			previous = interval
+			previous += interval
 		}
 		
 		toReturn
---
+++ EssentialsOfMetaheuristics/test/geneticProgrammingProblems/SymbolicRegressionSinXTests.groovy
@@ -0,0 +1,35 @@
+package geneticProgrammingProblems
+
+import functions.*
+import geneticTreeStructure.GeneticTree
+import geneticTreeStructure.InternalNode
+import geneticTreeStructure.NumericConstantNode
+import geneticTreeStructure.VariableNode
+import spock.lang.Specification
+
+class SymbolicRegressionSinXTests extends Specification {
+	def "testing getPoints"() {
+		def problem = new SymbolicRegressionSinX()
+		def givenPoints = problem.getPoints()
+		def actualPoints = [0.6283185307179586, 1.2566370614359172, 1.8849555921538759, 2.5132741228718345, 
+							3.141592653589793, 3.7699111843077517, 4.39822971502571, 5.026548245743669, 
+							5.654866776461628, 6.283185307179586]
+		
+		expect:
+		givenPoints == actualPoints
+	}
+	
+	def "testing quality"() {
+		def problem = new SymbolicRegressionSinX()
+		def geneticTree = new GeneticTree()
+		def functionSet = [new InternalNode(new Add(), []), new InternalNode(new Subtract(), [])]
+		def terminalSet = [new VariableNode("x"), new NumericConstantNode(83), new NumericConstantNode(1990), new NumericConstantNode(12)]
+		
+		def root = geneticTree.doFull([1, 5, functionSet, terminalSet])
+		def quality = problem.quality(root)
+		
+		expect:
+		quality >= 0
+		println(quality)
+	}
+}
